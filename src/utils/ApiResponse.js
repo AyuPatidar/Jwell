52f6b6672c84ced048643d2e0edec7a5aa8b39e9
Your Name <you@example.com>
2024-03-17 12:48:43 +0530

    add utils

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+export class ApiResponse {
+  constructor(statusCode, message = "Success", data) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+    this.success = this.success < 400;
+  }
+}
