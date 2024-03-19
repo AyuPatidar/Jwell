9c705b3c25968cdd3dbb3ea2cea0c017bdf1f9ae
Your Name <you@example.com>
2024-03-19 19:06:27 +0530

    Routes setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AgentsPage from "./pages/AgentsPage";
+
+const routes = [
+  { path: "/", element: <HomePage /> },
+  { path: "/agents", element: <AgentsPage /> },
+];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={route.element}
+          />
+        ))}
+      </Routes>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
