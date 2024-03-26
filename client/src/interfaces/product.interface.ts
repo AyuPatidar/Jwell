80a0bb61dc8d1d4d3a0733a12fc29e7305e482e0
Your Name <you@example.com>
2024-03-26 11:09:26 +0530

    Define product interface

---
+++ client/src/interfaces/product.interface.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  _id: string;
+  productType: string;
+  name: string;
+  tunch: number;
+  wastage: number;
+  weight: number;
+  weight_unit: string;
+  stone: string;
+  labour: string;
+  rate: number;
+  amount: number;
+  createdAt: string;
+  updatedAt: string;
+}
