440acfe5f1b1cbf7f360b020c148ee72d2a45fc4
Your Name <you@example.com>
2024-04-07 21:31:58 +0530

    UI Improvements

--- client/src/pages/ProductForm.tsx
+++ client/src/pages/ProductForm.tsx
@@ -1,28 +1,40 @@
-import { Grid } from "@mui/material";
 import Sidebar from "../components/Sidebar";
-import { useState } from "react";
 import { API_BaseUrl } from "../constants";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import * as yup from "yup";
+import { Box, Button } from "@mui/material";
+import { Form, Formik, FormikHelpers, FormikValues } from "formik";
 
 const ProductForm = () => {
   const navigate = useNavigate();
 
-  const [productType, setProductType] = useState("");
-  const [name, setName] = useState("");
-  const [stock, setStock] = useState(0);
+  const validationSchema = yup.object().shape({
+    productType: yup
+      .string()
+      .oneOf(["Gold", "Silver", "Stone"])
+      .required("Required"),
+    name: yup.string().required("Required"),
+    stock: yup.number().moreThan(-1),
+  });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = (
+    values: FormikValues,
+    actions: FormikHelpers<{
+      productType: string;
+      name: string;
+      stock: number;
+    }>
+  ) => {
     fetch(`${API_BaseUrl}/products/new-product`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        productType: productType,
-        name: name,
-        stock: stock,
+        productType: values.productType.toLowerCase(),
+        name: values.name.toLowerCase(),
+        stock: values.stock,
       }),
     })
       .then((res) => res.json())
@@ -36,54 +48,64 @@ const ProductForm = () => {
 
   return (
     <>
-      <Grid container>
-        <Grid
-          item
-          md={2}
-          lg={2}
+      <Sidebar />
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        height={"80vh"}
+      >
+        <Formik
+          initialValues={{
+            productType: "",
+            name: "",
+            stock: 0,
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => handleSubmit(values, actions)}
         >
-          <Sidebar />
-        </Grid>
-        <Grid
-          item
-          display={"flex"}
-          md={10}
-          lg={10}
-          justifyContent={"center"}
-          alignItems={"center"}
-        >
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="productType">Product Type: </label>
-            <input
-              type="text"
-              name="productType"
-              id="productType"
-              value={productType}
-              onChange={(e) => setProductType(e.target.value)}
-            />
-            <br />
-            <label htmlFor="name">Name: </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <br />
-            <label htmlFor="Stock">Stock: </label>
-            <input
-              type="text"
-              name="stock"
-              id="stock"
-              value={stock}
-              onChange={(e) => setStock(Number(e.target.value))}
-            />
-            <br />
-            <button type="submit">Submit</button>
-          </form>
-        </Grid>
-      </Grid>
+          {({ values, errors, handleChange }) => (
+            <Form>
+              <label htmlFor="productType">Product Type: </label>
+              <input
+                type="text"
+                name="productType"
+                id="productType"
+                value={values.productType}
+                onChange={handleChange}
+              />
+              <br />
+              <label htmlFor="name">Name: </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={values.name}
+                onChange={handleChange}
+              />
+              <br />
+              <label htmlFor="Stock">Stock: </label>
+              <input
+                type="text"
+                name="stock"
+                id="stock"
+                value={values.stock}
+                onChange={handleChange}
+              />
+              <br />
+
+              <Button
+                variant="contained"
+                size="large"
+                type="submit"
+              >
+                Submit
+              </Button>
+              <pre>{JSON.stringify({ values, errors }, null, 4)}</pre>
+            </Form>
+          )}
+        </Formik>
+      </Box>
     </>
   );
 };
