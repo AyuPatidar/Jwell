7bbb1451dccee2662fab2b03dcc4e32fc586e579
Your Name <you@example.com>
2024-04-12 21:21:33 +0530

    UI Improvements

--- client/src/components/UserForm.tsx
+++ client/src/components/UserForm.tsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
 import { IUser } from "../interfaces/user.interface";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { API_BaseUrl } from "../constants";
+import * as yup from "yup";
+import {
+  ErrorMessage,
+  Form,
+  Formik,
+  FormikHelpers,
+  FormikValues,
+} from "formik";
+import { Box, Button, Grid, TextField } from "@mui/material";
 
 const UserForm = ({ userType, user }: { userType: string; user?: IUser }) => {
   const navigate = useNavigate();
 
-  const [name, setName] = useState(user?.name || "");
-  const [address, setAddress] = useState(user?.address || "");
-  const [phoneNo, setPhoneNo] = useState(user?.phoneNo || "");
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const validationSchema = yup.object().shape({
+    name: yup.string().required("Required"),
+    address: yup.string(),
+    phoneNo: yup.string().required("Required"),
+  });
+
+  const handleSubmit = (
+    values: FormikValues,
+    actions: FormikHelpers<{
+      name: string;
+      address: string;
+      phoneNo: string;
+    }>
+  ) => {
     if (user) {
       fetch(`${API_BaseUrl}/users/${user._id}/update`, {
         method: "PATCH",
@@ -20,9 +37,9 @@ const UserForm = ({ userType, user }: { userType: string; user?: IUser }) => {
         },
         body: JSON.stringify({
           id: user._id,
-          name: name,
-          address: address,
-          phoneNo: phoneNo,
+          name: values.name,
+          address: values.address,
+          phoneNo: values.phoneNo,
         }),
       })
         .then((res) => res.json())
@@ -35,9 +52,9 @@ const UserForm = ({ userType, user }: { userType: string; user?: IUser }) => {
         },
         body: JSON.stringify({
           userType: userType,
-          name: name,
-          address: address,
-          phoneNo: phoneNo,
+          name: values.name,
+          address: values.address,
+          phoneNo: values.phoneNo,
         }),
       })
         .then((res) => res.json())
@@ -45,34 +62,111 @@ const UserForm = ({ userType, user }: { userType: string; user?: IUser }) => {
     }
     navigate(-1);
   };
+
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="name">Name: </label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <label htmlFor="phoneNo">Phone No.:</label>
-      <input
-        id="phoneNo"
-        type="text"
-        name="phoneNo"
-        value={phoneNo}
-        onChange={(e) => setPhoneNo(e.target.value)}
-      />
-      <label htmlFor="address">Address: </label>
-      <input
-        id="address"
-        type="text"
-        name="address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <Box
+      height={"80vh"}
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Formik
+        initialValues={{
+          name: user?.name || "",
+          address: user?.address || "",
+          phoneNo: user?.phoneNo || "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ values, errors, touched, handleChange, isSubmitting }) => (
+          <Form>
+            <Grid
+              container
+              direction={"column"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              spacing={2}
+            >
+              <Grid item>
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                >
+                  <TextField
+                    id="name"
+                    name="name"
+                    type="text"
+                    label="Name"
+                    value={values.name}
+                    onChange={handleChange}
+                    sx={{ width: "300px" }}
+                  />
+                  {errors.name && touched.name && (
+                    <span style={{ color: "red" }}>
+                      <ErrorMessage name="name" />
+                    </span>
+                  )}
+                </Box>
+              </Grid>
+              <Grid item>
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                >
+                  <TextField
+                    id="phoneNo"
+                    name="phoneNo"
+                    type="text"
+                    label="Phone No."
+                    value={values.phoneNo}
+                    onChange={handleChange}
+                    sx={{ width: "300px" }}
+                  />
+                  {errors.phoneNo && touched.phoneNo && (
+                    <span style={{ color: "red" }}>
+                      <ErrorMessage name="phoneNo" />
+                    </span>
+                  )}
+                </Box>
+              </Grid>
+              <Grid item>
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                >
+                  <TextField
+                    id="address"
+                    name="address"
+                    type="text"
+                    label="Address"
+                    value={values.address}
+                    onChange={handleChange}
+                    sx={{ width: "300px" }}
+                  />
+                  {errors.address && touched.address && (
+                    <span style={{ color: "red" }}>
+                      <ErrorMessage name="address" />
+                    </span>
+                  )}
+                </Box>
+              </Grid>
+              <Grid item>
+                <Button
+                  type="submit"
+                  size="large"
+                  variant="contained"
+                  disabled={isSubmitting}
+                >
+                  SUBMIT
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </Box>
   );
 };
 
